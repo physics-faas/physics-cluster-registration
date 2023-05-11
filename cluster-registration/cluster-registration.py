15801b9e8f4a1ad00368baf392fe745ff0fb221c
Luis Tomas Bolivar <ltomasbo@redhat.com>
2023-05-11 16:00:48 +0200

    Add support for deploying a job to measure energy consumption

--- cluster-registration/cluster-registration.py
+++ cluster-registration/cluster-registration.py
@@ -12,19 +12,27 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
 EVENT_TYPES = ("dev.knative.apiserver.resource.add",)
-MANIFEST_WORK_NAME = "cluster-registration"
-SERVICE_IP = "serviceIP"
 SERVICE_PORT = 5000
 SERVICE_NAMESPACE="open-cluster-management-agent"
 SERVICE_ACCOUNT="klusterlet-work-sa"
 PULL_SECRET="physics-harbor-pullsecret"
 RF_URL="https://semantics-block.apps.ocphub.physics-faas.eu/api/v2/cluster/register"
 RF_API_KEY="CHANGEME"
+MANIFEST_WORK_SEMANTICS = "cluster-registration-semantics"
+MANIFEST_WORK_ENERGY = "cluster-registration-energy"
 
-def deploy_manifest_work(namespace):
+manifest_work_map = {
+    MANIFEST_WORK_SEMANTICS: create_semantics_manifest_work,
+    MANIFEST_WORK_ENERGY: create_energy_manifest_work,
+}
+
+
+def deploy_manifest_work(namespace, manifest_type):
     #config.load_kube_config()
     config.load_incluster_config()
-    manifest_work = create_manifest_work()
+
+    manifest_work = manifest_work_map.get(manifest_type)
+    manifest_work()
 
     api = client.CustomObjectsApi()
     api.create_namespaced_custom_object(
@@ -35,7 +43,78 @@ def deploy_manifest_work(namespace):
         body=manifest_work,
     )
 
-def create_manifest_work():
+
+def create_energy_manifest_work():
+    physics_energy_pod = {
+        "apiVersion": "batch/v1",
+        "kind": "Job",
+        "metadata": {
+            "name": "energy-semantics",
+            "namespace": SERVICE_NAMESPACE
+        },
+        "spec": {
+            "template": {
+                "metadata": {
+                    "name": "energy-semantics"
+                },
+                "spec": {
+                    "containers": [
+                    {
+                        "name": "energy-semantics",
+                        "image": "progrium/stress",
+                        "command": ["sh", "-c"],
+                        "args": ["stress-ng --cpu 4 --vm-bytes 512M --vm-keep -t 180s"],
+                        "resources": {
+                            "limits": {
+                                "cpu": "1",
+                                "memory": "1Gi"
+                            },
+                            "requests": {
+                                "cpu": "0.5",
+                                "memory": "512Mi"
+                            }
+                        }
+                    }],
+                    "restartPolicy": "OnFailure"
+                }
+            }
+        }
+    }
+
+    physics_energy_r_id = {
+        "group": "batch",
+        "name": "energy-semantics",
+        "namespace": SERVICE_NAMESPACE,
+        "resource": "jobs",
+        "version": "v1"
+    }
+    physics_energy_feedback = {
+        "type": "JSONPaths",
+        "jsonPaths": [{
+            "name": "jobCompleted",
+            "path": ".status.completionTime"
+        }]
+    }
+
+    return {
+        "apiVersion": "work.open-cluster-management.io/v1",
+        "kind": "ManifestWork",
+        "metadata": {"name": MANIFEST_WORK_ENERGY},
+        "spec": {
+            "manifestConfigs": [{
+                "resourceIdentifier": physics_energy_r_id,
+                "feedbackRules": [physics_energy_feedback]
+            }],
+            "workload": {
+                "manifests": [
+                    physics_energy_pod
+                ]
+            }
+        }
+    }
+
+
+def create_semantics_manifest_work():
     # TODO: Change by actual pod
     physics_semantic_pod = {
         "apiVersion": "apps/v1",
@@ -116,7 +195,7 @@ def create_manifest_work():
     return {
         "apiVersion": "work.open-cluster-management.io/v1",
         "kind": "ManifestWork",
-        "metadata": {"name": MANIFEST_WORK_NAME},
+        "metadata": {"name": MANIFEST_WORK_SEMANTICS},
         "spec": {
             "manifestConfigs": [{
                 "resourceIdentifier": physics_semantic_r_id,
@@ -131,7 +210,8 @@ def create_manifest_work():
         }
     }
 
-def get_manifest_work_status(namespace):
+
+def get_manifest_work_status(namespace, manifest_name):
     config.load_incluster_config()
     api = client.CustomObjectsApi()
     return api.get_namespaced_custom_object_status(
@@ -139,7 +219,63 @@ def get_manifest_work_status(namespace):
         version="v1",
         namespace=namespace,
         plural="manifestworks",
-        name=MANIFEST_WORK_NAME)
+        name=manifest_name)
+
+
+def deploy_semantic_component(cluster_name):
+    # create a manifestwork on the cluster name namespace
+    deploy_manifest_work(cluster_name, MANIFEST_WORK_SEMANTICS)
+
+    # get the service ip (in a loop) from the manifestwork status
+    retries = 5
+    service_ip = None
+    while retries:
+        manifest_work_status = get_manifest_work_status(cluster_name, MANIFEST_WORK_SEMANTICS)
+        if manifest_work_status.get('status'):
+            manifests = manifest_work_status['status']['resourceStatus'].get('manifests', [])
+            for manifest in manifests:
+                status_feedback = manifest.get('statusFeedback')
+                if status_feedback:
+                    for value in status_feedback['values']:
+                        if value['name'] == 'serviceIP':
+                            service_ip = value['fieldValue']['string']
+                            break
+                if service_ip:
+                    break
+        if service_ip:
+            break
+        retries-=1
+        time.sleep(5)
+    return service_ip
+
+
+def deploy_energy_bench(cluster_name):
+    deploy_manifest_work(cluster_name, MANIFEST_WORK_ENERGY)
+
+    # Wait until Job finishes or 5 mins
+    retries = 20
+    completed = False
+    while retries:
+        manifest_work_status = get_manifest_work_status(cluster_name, MANIFEST_WORK_ENERGY)
+        if manifest_work_status.get('status'):
+            manifests = manifest_work_status['status']['resourceStatus'].get('manifests', [])
+            for manifest in manifests:
+                status_feedback = manifest.get('statusFeedback')
+                if status_feedback:
+                    for value in status_feedback['values']:
+                        if value['name'] == "jobCompleted":
+                            completed = True
+                            break
+                if completed:
+                    break
+        if completed:
+            break
+        retries-=1
+        time.sleep(15)
+    if completed:
+        return True
+    return False
+
 
 # create an endpoint at http://localhost:/8080/
 @app.route("/", methods=["POST"])
@@ -154,39 +290,32 @@ def home():
 
     # filter by event type (only "add" event is needed)
     if event_type not in EVENT_TYPES:
-       return "", 204
+       return "", 202
 
     # Get the name of the cluster
     cluster_name = event['name']
     app.logger.info('Cluster name is %s', cluster_name)
 
-    # create a manifestwork on the cluster name namespace
-    deploy_manifest_work(namespace=cluster_name)
+    # Steps:
+    # 1. Deploy energy bench and wait for execution completion
+    success = deploy_energy_bench(cluster_name)
+    if not success:
+        app.logger.error('Something went wrong while deploying the job to '
+                         'measure performance/energy')
+        return "TimeOut wating for Job completion", 500
 
-    # get the service ip (in a loop) from the manifestwork status
-    retries = 5
-    service_ip = None
-    while retries:
-        manifest_work_status = get_manifest_work_status(namespace=cluster_name)
-        if manifest_work_status.get('status'):
-            manifests = manifest_work_status['status']['resourceStatus'].get('manifests', [])
-            for manifest in manifests:
-                status_feedback = manifest.get('statusFeedback')
-                if status_feedback:
-                    for value in status_feedback['values']:
-                        if value['name'] == SERVICE_IP:
-                            service_ip = value['fieldValue']['string']
-                            break
-                if service_ip:
-                    break
-        if service_ip:
-            app.logger.info('The service IP is %s', service_ip)
-            app.logger.info('The service PORT is %s', SERVICE_PORT)
-            break
-        retries-=1
-        time.sleep(5)
+    # 2. Deploy the semantic component, with information about the energy bench
+    service_ip = deploy_semantic_component(cluster_name)
 
-    # query the RF with the cluster name and service IP
+    if not service_ip:
+        return "TimeOut waiting for Service IP", 500
+
+    app.logger.info('The service IP is %s', service_ip)
+    app.logger.info('The service PORT is %s', SERVICE_PORT)
+
+    # 3. Call the Semantic Component with the information about the pod
+
+    # 4. Call the RF with the information about the semantic service (IP)
     headers = {'X-API-Key': RF_API_KEY}
     cluster_info = {
         'clusterName': cluster_name,
